30316f3cdfc5559ac3f093ec20429e5e4d528d1a
jk.yang <yjkbako@gmail.com>
2023-11-26 18:19:05 +0900

    Header完成

--- app/index.js
+++ app/index.js
@@ -8,13 +8,47 @@ import {
   ScreenHeaderBtn,
   Welcome,
 } from "../components";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "white" },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.menu}
+              dimension='60%'
+              handlePress={() => router.push("/")}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={images.profile}
+              dimension='100%'
+              handlePress={() => router.push("/profile")}
+            />
+          ),
+          headerTitle: "",
+        }}
+      />
       <Text>Homess</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
